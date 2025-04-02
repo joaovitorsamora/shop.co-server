b2adf865d364ee49d5454db638c76664f17691b8
joaovitorsamora <jvsamora00@gmail.com>
2025-04-02 16:47:57 -0300

    update

--- server.mjs
+++ server.mjs
@@ -1,10 +1,13 @@
 import Jsonserver from "json-server"
 import express from "express"
+import cors from "cors"
 import path from "path"
 
 const server = Jsonserver.create()
 const router = Jsonserver.router("db.json")
 const middlewares = Jsonserver.defaults()
+
+server.use(cors())
 
 server.use("/images", express.static(path.join(process.cwd(), "public/images")));
 
@@ -15,3 +18,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running on port 3000')
 })
 
+export default server
