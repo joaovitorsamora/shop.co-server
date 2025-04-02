25e538282aab1263b7c8dbc3211b2521051af935
joaovitorsamora <jvsamora00@gmail.com>
2025-04-02 11:30:56 -0300

    update

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const server = Jsonserver.create()
 const router = Jsonserver.router("db.json")
 const middlewares = Jsonserver.defaults()
 
-server.use(middlewares)
+server.use("/images", JsonServer.static(path.join(__dirname, "public/images")));
 
+server.use(middlewares)
 server.use(router)
 
 server.listen(3000, () => {
